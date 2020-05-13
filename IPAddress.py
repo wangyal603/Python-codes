72d128cc964cd1fa77538f9dd9b3c279a4ffc194
Wangyal Tshering <65112142+wangyal603@users.noreply.github.com>
2020-05-13 22:28:15 +0530

    Create IPAddress.py

---
+++ IPAddress.py
@@ -0,0 +1,7 @@
+#Python Program to Get your IP Address
+
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+print("Your Device name is:" + hostname)
+print("Your Device IP Address is:" + IPAddr)
