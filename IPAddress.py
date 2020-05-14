d33ae3e7ed9056f474aaca25ed99394b0a5c27f0
Wangyal Tshering <wbhutia133@gmail.com>
2020-05-14 10:53:04 +0530

    Delete IPAddress

--- IPAddress.py
+++
@@ -1,7 +0,0 @@
-#Python Program to Get your IP Address
-
-import socket
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-print("Your Device name is:" + hostname)
-print("Your Device IP Address is:" + IPAddr)
