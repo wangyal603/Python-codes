9e3585b27af8ebf8722528ef830fa81fde49ceed
Wangyal Tshering <wbhutia133@gmail.com>
2020-05-19 18:21:29 +0530

    Create AIchatbot.py
    So, recently I've been working on this program and I didn't get bugs that I was expecting and I released it a bit early and will be releasing another program shortly.

---
+++ AIchatbot.py
@@ -0,0 +1,23 @@
+#python 3.7.1
+#simple AI chatbot
+print("hello")
+input()
+print("my name is chatbot")
+name=input("what is your name?")
+print("nice name")
+doing=input("are u working or studying")
+print(name,"are you",doing,"thats nice")
+input()
+print("ok")
+hero=input("who is your favourite hero")
+print("i also like that",hero)
+input()
+print("okk this was nice meeting ")
+print("byee")
+input()
+print("ok meet u again")
+
+print("______meeting is ended_______")
+
+#by
+#W.Tshering
